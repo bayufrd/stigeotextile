353a5fc6595341570c49f058299afbbf5b9852b8
marcelino0707 <marcelinoahong0707@gmail.com>
2025-03-13 01:09:56 +0700

    setup init package

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,10 @@
+import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import "./globals.css"; // Untuk Tailwind atau styling global
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={nunito.className}>
       <body className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-1 container mx-auto p-4">{children}</main>

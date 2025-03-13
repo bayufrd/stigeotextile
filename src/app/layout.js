42ef96b16667b7eba8072bb6f434828640ce1f44
marcelino0707 <marcelinoahong0707@gmail.com>
2025-03-13 23:28:53 +0700

    init for font family to root global

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={nunito.className}>
+    <html lang="en">
       <body className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-1 container mx-auto p-4">{children}</main>

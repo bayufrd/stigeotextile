b7b5008f2afe274c481facc7f6f84a252b7c06b8
marcelino0707 <marcelinoahong0707@gmail.com>
2025-03-16 18:14:13 +0700

    Add Header

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -8,10 +8,14 @@ export default function Navbar() {
     const [isVisible, setIsVisible] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isAtTop, setIsAtTop] = useState(true);
 
     useEffect(() => {
         const handleScroll = () => {
             const currentScrollY = window.scrollY;
+
+            setIsAtTop(currentScrollY === 0);
+
             if (currentScrollY > lastScrollY && currentScrollY > 50) {
                 setIsVisible(false);
             } else {
@@ -31,7 +35,8 @@ export default function Navbar() {
 
     return (
         <nav
-            className={`fixed top-0 left-0 w-full p-4 text-black shadow-md transition-transform duration-300 backdrop-blur-lg bg-white/75 z-50 ${isVisible ? "translate-y-0" : "-translate-y-full"}`}
+            className={`${isAtTop ? "absolute" : "fixed"} top-0 left-0 w-full p-4 text-white shadow-md transition-transform duration-300 ${isAtTop ? "bg-transparent" : "backdrop-blur-lg bg-white/75"} z-50 
+            ${isVisible ? "translate-y-0" : "-translate-y-full"}`}    
         >
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/" className="flex items-center">

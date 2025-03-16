5c1f875a04912fcfbf7935a5866395ad92930646
marcelino0707 <marcelinoahong0707@gmail.com>
2025-03-16 20:24:52 +0700

    Update Header & Navbar

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,11 +1,9 @@
 export default function Header() {
-    return (
-      <header className="relative w-full bg-gradient-to-r from-gray-800 to-gray-700 text-white py-25 px-4">
-        {/* Elemen dekorasi gambar
-        <div className="absolute bottom-0 right-1/4 w-40 h-40 bg-white rounded-full overflow-hidden border-4 border-gray-300 shadow-lg">
-          <img src="/header-image.jpg" alt="Header" className="w-full h-full object-cover" />
-        </div> */}
-      </header>
-    );
-  }
-  
+  return (
+    <header className="relative w-full py-25 px-4">
+      <div 
+        className="absolute inset-0 z-0 bg-cover bg-center bg-[url('https://i.pinimg.com/736x/80/ad/63/80ad631f67f14b858f04f8faab8cfeae.jpg')] brightness-50" 
+      ></div>
+    </header>
+  );
+}

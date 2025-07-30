98be16cd9408da2ce7c4b002b14ce6fc94303e77
bayufrd <55101246+bayufrd@users.noreply.github.com>
2025-07-31 04:54:50 +0700

    Fixing for Varcel

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,3 @@
-// // next.config.mjs
-// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   webpack: (config, { isServer }) => {
-//     if (process.env.ANALYZE_BUNDLE_SIZE === 'true') {
-//       config.plugins.push(
-//         new BundleAnalyzerPlugin({
-//           analyzerMode: 'server',
-//           analyzerPort: isServer ? 8888 : 8889,
-//           openAnalyzer: true,
-//         })
-//       );
-//     }
-//     return config;
-//   },
-// };
-
-// export default nextConfig;
-
-// next.config.mjs
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
